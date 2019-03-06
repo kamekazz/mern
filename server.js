78d3fe73d8aa364260e49c15025c52bec709d921
manuel taveras <taveras78@gmail.com>
2019-03-06 00:45:18 -0400

    make it better

--- server.js
+++ server.js
@@ -31,13 +31,16 @@ const userRouters = require('./routes/user.routes')
 
 app.use('/api/accounts',userRouters)
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'))
-
-    app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname,'build','index.html'))
-    })
-}
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 
 
 
